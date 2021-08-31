428e678b37cafff098e0b4f368d8bcd8aa3d6dac
Navin G V <gnavin@amazon.com>
2021-08-31 09:18:09 +0530

    adding 1299

---
+++ leetcode/editor/en/[34]Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,37 @@
+//Given an array of integers nums sorted in ascending order, find the starting 
+//and ending position of a given target value. 
+//
+// If target is not found in the array, return [-1, -1]. 
+//
+// You must write an algorithm with O(log n) runtime complexity. 
+//
+// 
+// Example 1: 
+// Input: nums = [5,7,7,8,8,10], target = 8
+//Output: [3,4]
+// Example 2: 
+// Input: nums = [5,7,7,8,8,10], target = 6
+//Output: [-1,-1]
+// Example 3: 
+// Input: nums = [], target = 0
+//Output: [-1,-1]
+// 
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// nums is a non-decreasing array. 
+// -10⁹ <= target <= 10⁹ 
+// 
+// Related Topics Array Binary Search 👍 6969 👎 230
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
