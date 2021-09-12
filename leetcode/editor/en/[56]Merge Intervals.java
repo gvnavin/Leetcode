5a2bdcc00b656246403b274a0204e7b3b11cc064
Navin G V <gnavin@amazon.com>
2021-09-13 00:03:35 +0530

    adding 56

--- leetcode/editor/en/[56]Merge Intervals.java
+++ leetcode/editor/en/[56]Merge Intervals.java
@@ -30,10 +30,117 @@
 // Related Topics Array Sorting 👍 9237 👎 428
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.TreeSet;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution56 {
+
+    static class ValAndFlag {
+        public int v;
+        public int f;
+        public int ind;
+        public int sum;
+
+        public ValAndFlag(int v, int f, int ind) {
+            this.v = v;
+            this.f = f;
+            this.ind = ind;
+            sum = 0;
+        }
+
+//        @Override
+//        public String toString() {
+//            return "ValAndFlag{" +
+//                    "v=" + v +
+//                    ", f=" + f +
+//                    ", ind=" + ind +
+//                    ", sum=" + sum +
+//                    '}';
+//        }
+    }
+
     public int[][] merge(int[][] intervals) {
-        return new int [1][1];
+        final TreeSet<ValAndFlag> valAndFlags = buildSortedIntervals(intervals);
+        ArrayList<ValAndFlag> stk = new ArrayList<>();
+
+        ArrayList<Integer> beg = new ArrayList<>();
+        ArrayList<Integer> end = new ArrayList<>();
+
+        for (ValAndFlag iv : valAndFlags) {
+            int stTopSum = getStkSum(stk);
+            iv.sum = stTopSum + iv.f;
+            stk.add(iv);
+
+            if (popAndPush(stk)) {
+                beg.add(stk.get(0).v);
+                end.add(stk.get(stk.size() - 1).v);
+                stk.clear();
+            }
+        }
+
+//        System.out.println("beg = " + beg);
+//        System.out.println("end = " + end);
+
+        int size = beg.size();
+//        System.out.println("size = " + size);
+        int[][] ret = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            ret[i][0] = beg.get(i);
+            ret[i][1] = end.get(i);
+        }
+
+//        for (int[] ints : ret) {
+//            for (int anInt : ints) {
+//                System.out.print(anInt + ",");
+//            }
+//            System.out.println();
+//        }
+
+        return ret;
+    }
+
+    private int getStkSum(ArrayList<ValAndFlag> stk) {
+        if (stk.isEmpty()) {
+            return 0;
+        }
+        return stk.get(stk.size()-1).sum;
+    }
+
+    private TreeSet<ValAndFlag> buildSortedIntervals(int[][] intervals) {
+        final TreeSet<ValAndFlag> valAndFlags = new TreeSet<>(getComparator());
+        for (int i = 0; i < intervals.length; i++) {
+            int[] iv = intervals[i];
+            valAndFlags.add(new ValAndFlag(iv[0], -1, i));
+            valAndFlags.add(new ValAndFlag(iv[1], 1, i));
+//            System.out.println("valAndFlags = " + valAndFlags);
+        }
+//        System.out.println("valAndFlags.size() = " + valAndFlags.size());
+        return valAndFlags;
+    }
+
+    private Comparator<ValAndFlag> getComparator() {
+        return (o1, o2) -> {
+            if (o1.v != o2.v) {
+                return o1.v - o2.v;
+            } else if (o1.v == o2.v && o1.f == o2.f) {
+                return o1.ind - o2.ind;
+            } else {
+                return o1.f - o2.f;
+            }
+        };
+    }
+
+    private boolean popAndPush(ArrayList<ValAndFlag> ivs) {
+        return !ivs.isEmpty() && ivs.get(ivs.size() - 1).sum == 0;
+    }
+
+    public static void main(String[] args) {
+        new Solution56().merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
+        new Solution56().merge(new int[][]{{1, 4}, {1, 5}});
+        new Solution56().merge(new int[][]{{1, 4}, {4, 5}});
+        new Solution56().merge(new int[][]{{1, 4}, {0, 2}, {3, 5}});
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
