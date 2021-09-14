a494e87fdb9d469e0f369b5ac5817f5929bb8901
Navin G V <gnavin@amazon.com>
2021-09-14 17:42:49 +0530

    adding 133

--- leetcode/editor/en/Node.java
+++ leetcode/editor/en/Node.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Node {
     public int val;
     public Node left;
     public Node right;
     public Node next;
+    public List<Node> neighbors;
 
-    public Node() {}
-
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
     public Node(int _val) {
         val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 
     public Node(int _val, Node _left, Node _right, Node _next) {
